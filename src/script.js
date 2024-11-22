0cb5f460730c457a426da7d213faaa2b6ad8f719
rnagripino <rnagripino@gmail.com>
2024-11-21 18:04:32 -0800

    test

--- src/script.js
+++ src/script.js
@@ -3,8 +3,7 @@ function displayTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
 
-  console.log(response.data.)
-
+  console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
